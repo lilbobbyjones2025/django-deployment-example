b9676d5fea5318310c0f85a41dc01bba06eeb784
Big Bob <bigbobbyjones2025@gmail.com>
2018-12-23 15:54:38 -0600

    first push

---
+++ basic_app/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context_dict = {'text': "Hello World!",'number':10}
+    return render(request,'basic_app/index.html',context_dict)
+
+def other(request):
+    return render(request,'basic_app/other.html')
+
+def relative(request):
+    return render(request,'basic_app/relative_url_templates.html')
